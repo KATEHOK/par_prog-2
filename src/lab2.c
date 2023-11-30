f4c9e09bc86b83439f89e2ce72e9f3bcedd2188c
Katehok <a.timin2003@mail.ru>
2023-11-30 18:04:57 +0300

    Added report-files

--- src/lab2.c
+++ src/lab2.c
@@ -9,25 +9,30 @@ struct response {
 };
 
 void search(
+    const int threads,
     const unsigned int* array,
     const unsigned int target,
     const int count,
     struct response* resp)
 {
     double start, end;
-    int index = -1;
-
+    int i, index = count;
     start = omp_get_wtime();
-    for (int i = 0; i < count; i++)
+
+    #pragma omp parallel num_threads(threads) reduction(min: index) private(i)
     {
-        if (array[i] == target)
+        #pragma omp for
+        for (i = 0; i < count; i++)
         {
-            index = i;
-            break;
+            if (array[i] == target)
+            {
+                index = i;
+                break;
+            }
         }
     }
-    end = omp_get_wtime();
 
+    end = omp_get_wtime();
     resp->index = index;
     resp->duration = end - start;
 }
@@ -35,23 +40,59 @@ void search(
 int main(int argc, char** argv)
 {
     const int count = 1000000000;
-    unsigned int target;
+    const int targets_count = 50;
+    int available_threads;
     unsigned int* array;
-
+    unsigned int* targets;
     struct response resp;
-
-    /* Generate the random array */
-    array = (unsigned int*)malloc(count*sizeof(unsigned int));
-    for(int i = 0; i < count; ++i) rand_s(array + i);
-
-    for (int i = 0; i < 20; ++i) {
-        rand_s(&target);
-        search(array, target, count, &resp);
-        printf("____________________\n");
-        if (resp.index >= 0) printf("Found occurence of %u at index %d;\n", target, resp.index);
-        else printf("Number %u was not found;\n", target);
-        printf("Time spent: %f\n", resp.duration);
+    FILE* file;
+    errno_t status = fopen_s(&file, "../par_prog-2/report/data.txt", "w");
+    if (status != 0) {
+        printf("File was not opened!\nError code: %d\n", status);
+        return 0;
     }
 
+    available_threads = omp_get_num_procs();
+    printf("OpenMP: %d; Threads: %d\n", _OPENMP, available_threads);
+
+    /* Generate random array and targets*/
+    array = (unsigned int*)malloc(count*sizeof(unsigned int));
+    targets = (unsigned int*)malloc(targets_count * sizeof(unsigned int));
+    for (int i = 0; i < count || i < targets_count; ++i) {
+        if (i < count) rand_s(array + i);
+        if (i < targets_count) rand_s(targets + i);
+    }
+
+    // For all counts of threads
+    for (int threads = 1; threads <= available_threads; ++threads) {
+
+        status = fprintf(file, "\nThreads: %d\n#\tTIME\tFOUND\n", threads);
+        if (status < 0) printf("Data was not written!\nError code: %d\n", status);
+
+        // For all counts of targets
+        for (int i = 0; i < targets_count; ++i) {
+            int found = 0;
+
+            // main part
+            search(threads, array, targets[i], count, &resp);
+
+            // std output
+            printf("[%d:%d]____________________\n", threads, i + 1);
+            if (resp.index < count) {
+                printf("%u FOUND at %d;\n", targets[i], resp.index);
+                ++found;
+            }
+            else printf("%u NOT FOUND;\n", targets[i]);
+            printf("TIME: %f\n\n", resp.duration);
+
+            // file output
+            status = fprintf(file, "%d\t%f\t%d\n", i + 1, resp.duration, found);
+            if (status < 0) printf("Data was not written!\nError code: %d\n", status);
+        }
+    }
+
+    free(array);
+    free(targets);
+    fclose(file);
     return(0);
 }
